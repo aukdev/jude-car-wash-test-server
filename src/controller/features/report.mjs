9787b7e32a965ce9c87010453b2974717ca752d3
aukdev <amilaupul344@outlook.com>
2024-10-02 09:37:57 +0530

    update

--- src/controller/features/report.mjs
+++ src/controller/features/report.mjs
@@ -60,6 +60,45 @@ export const getAllMonthly = async (page, size) => {
   }
 };
 
+// get All of this month for calc
+export const getAllCalcMonthly = async () => {
+  const startOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0,
+    23,
+    59,
+    59,
+    999
+  );
+
+  try {
+    const data = await DB.invoice.findMany({
+      orderBy: {
+        id: "desc",
+      },
+      where: {
+        StartingPoint: false,
+        PaymentStatus: "success",
+        createdAt: {
+          gte: startOfMonth,
+          lte: endOfMonth,
+        },
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+};
+
 // get All pos of this month
 export const getPOSMonthly = async (page, size) => {
   const offset = (Number(page) - 1) * Number(size);
@@ -121,6 +160,47 @@ export const getPOSMonthly = async (page, size) => {
     return "error";
   }
 };
+
+// get All pos of this month calc
+export const getPOSCalcMonthly = async () => {
+  const startOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0,
+    23,
+    59,
+    59,
+    999
+  );
+
+  try {
+    const data = await DB.invoice.findMany({
+      orderBy: {
+        id: "desc",
+      },
+      where: {
+        StartingPoint: false,
+        PaymentStatus: "success",
+        PaymentMethod: "POS",
+        createdAt: {
+          gte: startOfMonth,
+          lte: endOfMonth,
+        },
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+};
+
 // get All Cash of this month
 export const getCashMonthly = async (page, size) => {
   const offset = (Number(page) - 1) * Number(size);
@@ -182,6 +262,47 @@ export const getCashMonthly = async (page, size) => {
     return "error";
   }
 };
+
+// get All Cash of this month calc
+export const getCashCalcMonthly = async () => {
+  const startOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0,
+    23,
+    59,
+    59,
+    999
+  );
+
+  try {
+    const data = await DB.invoice.findMany({
+      orderBy: {
+        id: "desc",
+      },
+      where: {
+        StartingPoint: false,
+        PaymentStatus: "success",
+        PaymentMethod: "Cash",
+        createdAt: {
+          gte: startOfMonth,
+          lte: endOfMonth,
+        },
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+};
+
 // get All bank transfer of this month
 export const getBankTransferMonthly = async (page, size) => {
   const offset = (Number(page) - 1) * Number(size);
@@ -238,6 +359,134 @@ export const getBankTransferMonthly = async (page, size) => {
     });
 
     return { data, count, page, pageSize: size };
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+};
+
+// get All bank transfer of this month calc
+export const getBankTransferCalcMonthly = async () => {
+  const startOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0,
+    23,
+    59,
+    59,
+    999
+  );
+
+  try {
+    const data = await DB.invoice.findMany({
+      orderBy: {
+        id: "desc",
+      },
+      where: {
+        StartingPoint: false,
+        PaymentStatus: "success",
+        PaymentMethod: "Bank Transfer",
+        createdAt: {
+          gte: startOfMonth,
+          lte: endOfMonth,
+        },
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+};
+
+// get jobs monthly
+export const getJobsMonthly = async (team) => {
+  const startOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0,
+    23,
+    59,
+    59,
+    999
+  );
+
+  try {
+    const data = await DB.booking.findMany({
+      orderBy: {
+        id: "desc",
+      },
+      include: {
+        Client: {
+          select: { Name: true },
+        },
+        Invoice: {
+          select: {
+            id: true,
+          },
+        },
+      },
+      where: {
+        Date: {
+          gte: startOfMonth,
+          lte: endOfMonth,
+        },
+        BookingStatus: {
+          not: "cancel",
+        },
+        TeamId: {
+          in: team,
+        },
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+};
+
+// get all booking by customer monthly
+export const getMonthlyBookingForCustomer = async (clientId) => {
+  const startOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0,
+    23,
+    59,
+    59,
+    999
+  );
+  try {
+    const data = await DB.booking.findMany({
+      where: {
+        ClientId: Number(clientId),
+        Date: {
+          gte: startOfMonth,
+          lte: endOfMonth,
+        },
+      },
+      orderBy: { id: "desc" },
+    });
+
+    return data;
   } catch (error) {
     console.log(error);
     return "error";
--- src/routes/features/report.mjs
+++ src/routes/features/report.mjs
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import {
+  getAllCalcMonthly,
   getAllCancelBookingMonthly,
   getAllMonthly,
+  getBankTransferCalcMonthly,
   getBankTransferMonthly,
+  getCashCalcMonthly,
   getCashMonthly,
+  getJobsMonthly,
+  getMonthlyBookingForCustomer,
+  getPOSCalcMonthly,
   getPOSMonthly,
 } from "../../controller/features/report.mjs";
 import { HTTPSTATUS, HTTPSTATUS_MSG } from "../../const/http-server-config.mjs";
@@ -85,6 +91,130 @@ reportRouter.get("/get-bank-transfer-monthly", async (c, w) => {
   const page = c.query.page;
   const size = c.query.size;
   const data = await getBankTransferMonthly(page, size);
+
+  if (data === "error") {
+    w.status(HTTPSTATUS.SERVER_ERROR).json(
+      clientResponse(
+        RESPONSE.ERROR,
+        HTTPSTATUS.SERVER_ERROR,
+        undefined,
+        HTTPSTATUS_MSG.SERVER_ERROR
+      )
+    );
+    return;
+  }
+
+  w.status(HTTPSTATUS.OK).json(
+    clientResponse(RESPONSE.SUCCESS, HTTPSTATUS.OK, data, undefined)
+  );
+});
+
+//get all monthly invoice calc
+reportRouter.get("/get-all-monthly-calc", async (_, w) => {
+  const data = await getAllCalcMonthly();
+
+  if (data === "error") {
+    w.status(HTTPSTATUS.SERVER_ERROR).json(
+      clientResponse(
+        RESPONSE.ERROR,
+        HTTPSTATUS.SERVER_ERROR,
+        undefined,
+        HTTPSTATUS_MSG.SERVER_ERROR
+      )
+    );
+    return;
+  }
+
+  w.status(HTTPSTATUS.OK).json(
+    clientResponse(RESPONSE.SUCCESS, HTTPSTATUS.OK, data, undefined)
+  );
+});
+
+//get all pos monthly invoice calc
+reportRouter.get("/get-pos-monthly-calc", async (_, w) => {
+  const data = await getPOSCalcMonthly();
+
+  if (data === "error") {
+    w.status(HTTPSTATUS.SERVER_ERROR).json(
+      clientResponse(
+        RESPONSE.ERROR,
+        HTTPSTATUS.SERVER_ERROR,
+        undefined,
+        HTTPSTATUS_MSG.SERVER_ERROR
+      )
+    );
+    return;
+  }
+
+  w.status(HTTPSTATUS.OK).json(
+    clientResponse(RESPONSE.SUCCESS, HTTPSTATUS.OK, data, undefined)
+  );
+});
+
+//get all cash monthly invoice calc
+reportRouter.get("/get-cash-monthly-calc", async (_, w) => {
+  const data = await getCashCalcMonthly();
+
+  if (data === "error") {
+    w.status(HTTPSTATUS.SERVER_ERROR).json(
+      clientResponse(
+        RESPONSE.ERROR,
+        HTTPSTATUS.SERVER_ERROR,
+        undefined,
+        HTTPSTATUS_MSG.SERVER_ERROR
+      )
+    );
+    return;
+  }
+
+  w.status(HTTPSTATUS.OK).json(
+    clientResponse(RESPONSE.SUCCESS, HTTPSTATUS.OK, data, undefined)
+  );
+});
+
+// get jobs monthly
+reportRouter.get("/get-jobs-monthly", async (c, w) => {
+  const data = await getJobsMonthly(
+    `${c.query.team}`.split(",").map((d) => Number(d))
+  );
+  if (data === "error") {
+    w.status(HTTPSTATUS.SERVER_ERROR).json(
+      clientResponse(
+        RESPONSE.ERROR,
+        HTTPSTATUS.SERVER_ERROR,
+        undefined,
+        HTTPSTATUS_MSG.SERVER_ERROR
+      )
+    );
+    return;
+  }
+  w.status(HTTPSTATUS.OK).json(
+    clientResponse(RESPONSE.SUCCESS, HTTPSTATUS.OK, data, undefined)
+  );
+});
+
+// get jobs monthly
+reportRouter.get("/get-customer-bookings-monthly/:clientId", async (c, w) => {
+  const data = await getMonthlyBookingForCustomer(c.params.clientId);
+  if (data === "error") {
+    w.status(HTTPSTATUS.SERVER_ERROR).json(
+      clientResponse(
+        RESPONSE.ERROR,
+        HTTPSTATUS.SERVER_ERROR,
+        undefined,
+        HTTPSTATUS_MSG.SERVER_ERROR
+      )
+    );
+    return;
+  }
+  w.status(HTTPSTATUS.OK).json(
+    clientResponse(RESPONSE.SUCCESS, HTTPSTATUS.OK, data, undefined)
+  );
+});
+
+//get all bank transfer monthly invoice calc
+reportRouter.get("/get-bank-transfer-monthly-calc", async (_, w) => {
+  const data = await getBankTransferCalcMonthly();
 
   if (data === "error") {
     w.status(HTTPSTATUS.SERVER_ERROR).json(
