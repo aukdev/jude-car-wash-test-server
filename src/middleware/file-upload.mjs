9679f969e561dcbd768a20f72cd3dc9802e8adf6
aukdev <amilaupul344@outlook.com>
2024-10-10 21:31:44 +0530

    upload

--- src/middleware/file-upload.mjs
+++ src/middleware/file-upload.mjs
@@ -7,7 +7,7 @@ const __dirname = import.meta.dirname;
 const storage = diskStorage({
   destination: join(__dirname, "..", "public"),
   filename: (_, f, cb) => {
-    console.log(f.originalname);
+    console.log(f.fieldname);
     console.log(f.mimetype);
     console.log(`${Date.now()}_${f.fieldname}${extname(f.originalname)}`);
     cb(null, `${Date.now()}_${f.fieldname}${extname(f.originalname)}`);
